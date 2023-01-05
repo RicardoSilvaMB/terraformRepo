288a3a17de988c97ff6542f38d6dd1bd4de461cf
Ricardo Silva <ricardo.silvafx@outlook.com>
2023-01-05 15:13:16 +0000

    Applying best practices

--- main.tf
+++ main.tf
@@ -1,17 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.27"
-    }
-  }
-}
-
-provider "aws" {
-  profile = "terraform"
-  region  = "eu-west-1"
-}
-
 resource "aws_vpc" "vpc1" {
     cidr_block = "10.0.0.0/16"
 
