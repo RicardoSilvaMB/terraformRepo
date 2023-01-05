fbfd59f226f2fe10c47018933158e243cfc21cf3
Ricardo Silva <ricardo.silvafx@outlook.com>
2023-01-05 14:45:56 +0000

    Updated content

--- main.tf
+++ main.tf
@@ -1,3 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-west-1"
+}
+
 resource "aws_vpc" "vpc1" {
     cidr_block = "10.0.0.0/16"
 
