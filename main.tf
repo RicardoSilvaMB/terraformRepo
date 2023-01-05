d895dd04f5d3493241d96c21901ec44fa844be00
Ricardo Silva <you@example.com>
2023-01-05 16:31:12 +0000

    Commit with 2 Subnets created

--- main.tf
+++ main.tf
@@ -5,3 +5,20 @@ resource "aws_vpc" "vpc1" {
         Name = "vpc1"
     }
 }
+resource "aws_subnet" "subnet1" {
+  vpc_id     = "vpc-0af2c37534243e1d3"
+  cidr_block = "172.31.50.0/24"
+
+  tags = {
+    Name = "subnet1"
+  }
+}
+
+resource "aws_subnet" "subnet2" {
+  vpc_id     = aws_vpc.vpc1.id
+  cidr_block = "10.0.43.0/24"
+
+  tags = {
+    Name = "subnet2"
+  }
+}
